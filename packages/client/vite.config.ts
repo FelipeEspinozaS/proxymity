563cc89100d1bfbe0f42c75391c0784cafc7cef1
wintermelyn <matas.seplveda@uc.cl>
2025-12-21 11:11:12 -0300

    feat: add path aliasing for src directory in TypeScript and Vite configuration

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      '@proxymity/shared': path.resolve(__dirname, '../shared/src/index.ts'),
+      "@": path.resolve(__dirname, "./src"),
+      '@proxymity/shared': path.resolve(__dirname, '../shared/src/index.ts')
     },
   },
   server: {
